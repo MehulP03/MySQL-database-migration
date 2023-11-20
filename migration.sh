d2f3f0dd0fba46b0456afb60a7672fac89b5de71
Mehul Patangya <mehuljainind@gmail.com>
2023-11-20 15:37:47 +0530

    added config.yaml for configuration and updated redme file

--- migration.sh
+++ migration.sh
@@ -1,53 +1,39 @@
 #!/bin/bash
 
-#configuration of source db take input through terminal
-echo "Enter the source db host/DNS"
-read source_db_host
-echo "Enter the source db user"
-read source_db_user
-echo "Enter the source db password"
-read source_db_password
-echo "Enter the source db name"
-read source_db_name
+perform_migration(){
+    source_db_host=$(yq '.source_database.db_host' config.yaml)
+    source_db_user=$(yq '.source_database.db_user' config.yaml)
+    source_db_password=$(yq '.source_database.db_password' config.yaml)
+    source_db_name=$(yq '.source_database.db_name' config.yaml)
 
-#hard code
-# source_db_host=""
-# source_db_user=""
-# source_db_password=""
-# source_db_name=""
+    target_db_host=$(yq '.destination_database.db_host' config.yaml)
+    target_db_user=$(yq '.destination_database.db_user' config.yaml)
+    target_db_password=$(yq '.destination_database.db_password' config.yaml)
+    target_db_name=$(yq '.destination_database.db_name' config.yaml)
 
+    mysqldump -h "$source_db_host" -u "$source_db_user" -p"$source_db_password" "$source_db_name" > backup.sql
 
-#configuration of tarfet db take input through terminal
-echo "Enter target db host/DNS"
-read target_db_host
-echo "target db user"
-read target_db_user
-echo "Enter target db password"
-read target_db_password
-echo "Enter the target db name"
-read target_db_name
+    #IN CASE OF RDS  created a backup of orignal file backup.sql>backup.sql.bak and comment this lines
+    sed -i.bak -e '/SET @MYSQLDUMP_TEMP_LOG_BIN/ s/^/-- /' \
+        -e '/SET @@SESSION.SQL_LOG_BIN;/ s/^/-- /' \
+        -e '/SET @@GLOBAL.GTID_PURGED/ s/^/-- /' \
+        -e '/SET @@SESSION.SQL_LOG_BIN/ s/^/-- /' \
+        backup.sql
 
-# #hard code
-# target_db_host=""
-# target_db_user=""
-# target_db_password=""
-# target_db_name=""
+    mysql -h "$target_db_host" -u "$target_db_user" -p"$target_db_password" "$target_db_name" < backup.sql
 
-mysqldump -h "$source_db_host" -u "$source_db_user" -p"$source_db_password" "$source_db_name" > backup.sql
+    #removing the backup file
+    rm backup.sql
 
-#IN CASE OF RDS  created a backup of orignal file backup.sql>backup.sql.bak and comment this lines
-sed -i.bak -e '/SET @MYSQLDUMP_TEMP_LOG_BIN/ s/^/-- /' \
-       -e '/SET @@SESSION.SQL_LOG_BIN;/ s/^/-- /' \
-       -e '/SET @@GLOBAL.GTID_PURGED/ s/^/-- /' \
-       -e '/SET @@SESSION.SQL_LOG_BIN/ s/^/-- /' \
-       backup.sql
+    #remove the the backup of orignal file 
+    # rm backup.sql.bak
 
-mysql -h "$target_db_host" -u "$target_db_user" -p"$target_db_password" "$target_db_name" < backup.sql
+    echo "Database migration completed successfully!"
+}
 
-#removing the backup file
-rm backup.sql
-
-#remove the the backup of orignal file 
-# rm backup.sql.bak
-
-echo "Database migration completed successfully!"
+if [ -f "config.yaml" ]; then
+    perform_migration
+else
+    echo "Error: config.yaml not found!"
+    exit 1
+fi
--- readme.md
+++ readme.md
@@ -1,12 +1,48 @@
+# Data Migration Using Shell Script
 ## Overview
 This shell script is designed to facilitate data migration from one location to another using a simple and customizable approach. It provides a straightforward method for copying or transferring data between different database providers.
 
+## Prerequisites
+### Shell Environment:
+Ensure that the script is executed in a shell environment that supports shell scripting (e.g., Bash)
+### `yq` Tool:
+Install the `yq` tool for processing YAML files. 
+```
+brew install yq 
+```
+
+## Installation
+### Clone the Repository:
+Clone this repository to your local environment.
+Note : Make sure you are inside repo folder created on local machine
+### Set Permissions: 
 Make the script executable by running the following command:
-```chmod +x migration.sh```
+```
+chmod +x migration.sh
+```
+
+## Configuration
+` config.yaml ` Structure
+
+```
+source_database:
+  db_host: <source_endpoint>
+  db_user: <source_username>
+  db_password: <source_password>
+  db_name: <source_database_name>
+
+destination_database:
+  db_host: <destination_endpoint>
+  db_user: <destination_username>
+  db_password: <destination_password>
+  db_name: <destination_database_name>
+```
+### NOTE:-
+User need to Configure `source_database` and `destination_database` 
+
+## Usage
 
 Then you can execute the script:
-```./migration.sh```
-
-## Execuation
-User need to provide Source endpoint, username , password and database name.
-As well as user need to provide Destination endpoint, username, password and database name.
+```
+./migration.sh
+```
